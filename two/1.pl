7ae0f6b1f05e29b3ddfd4df7ffffa39d3d0e3162
ben hengst <ben.hengst@gmail.com>
2012-11-25 22:32:13 -0800

    automated

--- two/1.pl
+++ two/1.pl
@@ -7,7 +7,7 @@ use Data::Dumper; sub D(@){ warn Dumper(@_) };
 sub b_chop{
   my $goal = shift;
   my @opts = @{$_[0]};
-  my ($first,$last) = 0, $#opts;
+  my ($first,$mix,$last) = (0, int($#opts/2), $#opts);
 D [$first,$last]
   my $mid = int($last/2); # half
 }
