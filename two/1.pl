d9cb445cbb0946ed7309b6ece98a3992987ce44b
ben hengst <ben.hengst@gmail.com>
2012-11-25 22:09:13 -0800

    automated

---
+++ two/1.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl 
+use strict;
+use warnings;
+use Test::Most qw{no_plan};
+
+is -1, chop(3, []);
+is -1, chop(3, [1]);
+is 0,  chop(1, [1]);
+
+is 0,  chop(1, [1, 3, 5]);
+is 1,  chop(3, [1, 3, 5]);
+is 2,  chop(5, [1, 3, 5]);
+is -1, chop(0, [1, 3, 5]);
+is -1, chop(2, [1, 3, 5]);
+is -1, chop(4, [1, 3, 5]);
+is -1, chop(6, [1, 3, 5]);
+
+is 0,  chop(1, [1, 3, 5, 7]);
+is 1,  chop(3, [1, 3, 5, 7]);
+is 2,  chop(5, [1, 3, 5, 7]);
+is 3,  chop(7, [1, 3, 5, 7]);
+is -1, chop(0, [1, 3, 5, 7]);
+is -1, chop(2, [1, 3, 5, 7]);
+is -1, chop(4, [1, 3, 5, 7]);
+is -1, chop(6, [1, 3, 5, 7]);
+is -1, chop(8, [1, 3, 5, 7]);
+
+
+
