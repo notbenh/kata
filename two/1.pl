0a61cfe3d878de4afdbaf5c89f15614662d9efd8
ben hengst <ben.hengst@gmail.com>
2012-11-25 22:10:21 -0800

    automated

--- two/1.pl
+++ two/1.pl
@@ -3,29 +3,29 @@ use strict;
 use warnings;
 use Test::Most qw{no_plan};
 
-sub chop{};
 
-is -1, chop(3, []);
-is -1, chop(3, [1]);
-is 0,  chop(1, [1]);
 
-is 0,  chop(1, [1, 3, 5]);
-is 1,  chop(3, [1, 3, 5]);
-is 2,  chop(5, [1, 3, 5]);
-is -1, chop(0, [1, 3, 5]);
-is -1, chop(2, [1, 3, 5]);
-is -1, chop(4, [1, 3, 5]);
-is -1, chop(6, [1, 3, 5]);
+is -1, b_chop(3, []);
+is -1, b_chop(3, [1]);
+is 0,  b_chop(1, [1]);
 
-is 0,  chop(1, [1, 3, 5, 7]);
-is 1,  chop(3, [1, 3, 5, 7]);
-is 2,  chop(5, [1, 3, 5, 7]);
-is 3,  chop(7, [1, 3, 5, 7]);
-is -1, chop(0, [1, 3, 5, 7]);
-is -1, chop(2, [1, 3, 5, 7]);
-is -1, chop(4, [1, 3, 5, 7]);
-is -1, chop(6, [1, 3, 5, 7]);
-is -1, chop(8, [1, 3, 5, 7]);
+is 0,  b_chop(1, [1, 3, 5]);
+is 1,  b_chop(3, [1, 3, 5]);
+is 2,  b_chop(5, [1, 3, 5]);
+is -1, b_chop(0, [1, 3, 5]);
+is -1, b_chop(2, [1, 3, 5]);
+is -1, b_chop(4, [1, 3, 5]);
+is -1, b_chop(6, [1, 3, 5]);
+
+is 0,  b_chop(1, [1, 3, 5, 7]);
+is 1,  b_chop(3, [1, 3, 5, 7]);
+is 2,  b_chop(5, [1, 3, 5, 7]);
+is 3,  b_chop(7, [1, 3, 5, 7]);
+is -1, b_chop(0, [1, 3, 5, 7]);
+is -1, b_chop(2, [1, 3, 5, 7]);
+is -1, b_chop(4, [1, 3, 5, 7]);
+is -1, b_chop(6, [1, 3, 5, 7]);
+is -1, b_chop(8, [1, 3, 5, 7]);
 
 
 
