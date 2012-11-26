4ca7efb2142365931ac9642dda7682e0519e3389
ben hengst <ben.hengst@gmail.com>
2012-11-25 22:41:16 -0800

    automated

--- two/1.pl
+++ two/1.pl
@@ -7,10 +7,11 @@ use Data::Dumper; sub D(@){ warn Dumper(@_) };
 sub b_chop{
   my $goal = shift;
   my @opts = @{$_[0]};
-  my ($first,$mid,$last) = (0, int($#opts/2), $#opts);
+  my ($first,$last) = (0, $#opts);
   my $i;
   while($first != $last) {
     die 'error' if $i++ > 10; # failsafe
+    my $mid = int( ($last-$first)/2 );
     D [$first,$mid,$last];
     return -1 unless defined $opts[$mid];
     return $mid if $goal  == $opts[$mid];
