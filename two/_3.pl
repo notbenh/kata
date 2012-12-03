85f2b961b00dec67cd5c478fbbb24edb798d290c
ben hengst <ben.hengst@gmail.com>
2012-12-02 21:20:56 -0800

    automated

--- two/_3.pl
+++ two/_3.pl
@@ -4,7 +4,7 @@ use warnings;
 use Test::Most qw{no_plan};
 use Data::Dumper; sub D(@){ warn Dumper(@_) };
 
-sub b_chop{ join '', _b_chop(@_) }
+sub b_chop{ warn join '', _b_chop(@_) }
 
 sub _b_chop {
   my $goal = shift;
