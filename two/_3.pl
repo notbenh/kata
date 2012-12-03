8dae267e18b20e9828b82414dc1525255cb71c01
ben hengst <ben.hengst@gmail.com>
2012-12-02 21:02:32 -0800

    automated

---
+++ two/_3.pl
@@ -0,0 +1,39 @@
+#!/usr/bin/perl 
+use strict;
+use warnings;
+use Test::Most qw{no_plan};
+use Data::Dumper; sub D(@){ warn Dumper(@_) };
+
+sub b_chop{
+  my $goal = shift;
+  my @opts = @{$_[0]};
+  #D {IN => [$goal, \@opts]};
+  my @first = splice @opts, 0, int($#opts/2);
+  D {F => \@first, S => \@opts};
+}
+
+
+is -1, b_chop(3, []);
+is -1, b_chop(3, [1]);
+is 0,  b_chop(1, [1]);
+
+is 0,  b_chop(1, [1, 3, 5]);
+is 1,  b_chop(3, [1, 3, 5]);
+is 2,  b_chop(5, [1, 3, 5]);
+is -1, b_chop(0, [1, 3, 5]);
+is -1, b_chop(2, [1, 3, 5]);
+is -1, b_chop(4, [1, 3, 5]);
+is -1, b_chop(6, [1, 3, 5]);
+
+is 0,  b_chop(1, [1, 3, 5, 7]);
+is 1,  b_chop(3, [1, 3, 5, 7]);
+is 2,  b_chop(5, [1, 3, 5, 7]);
+is 3,  b_chop(7, [1, 3, 5, 7]);
+is -1, b_chop(0, [1, 3, 5, 7]);
+is -1, b_chop(2, [1, 3, 5, 7]);
+is -1, b_chop(4, [1, 3, 5, 7]);
+is -1, b_chop(6, [1, 3, 5, 7]);
+is -1, b_chop(8, [1, 3, 5, 7]);
+
+
+
